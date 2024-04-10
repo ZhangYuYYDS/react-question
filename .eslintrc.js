ed86d383148c549894bb4de7084f69d51aecc320
zhangyu <1015371461@qq.com>
2024-04-10 21:19:07 +0800

    feat:init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended', // 用于eslint推荐规则
+    'plugin:prettier/recommended', // 用于prettier推荐规则
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: { 'comma-dangle': 'off' }
+}
