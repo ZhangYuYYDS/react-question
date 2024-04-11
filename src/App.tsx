e7b4354d204efd04b56506885df4a094ef56bd5d
zhangyu <1015371461@qq.com>
2024-04-11 21:48:06 +0800

    feat: hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 // import List1 from './List1'
 import List2 from './List2'
+// import useGetInfo from './hooks/useGetInfo'
 
 function App() {
+  // const { loading, info } = useGetInfo()
   // 列表页
   return (
     // fragment
     <>
       {/* <List1 /> */}
       <List2 />
+      {/* <p>{loading ? 'loading中' : info}</p> */}
     </>
   )
 }
