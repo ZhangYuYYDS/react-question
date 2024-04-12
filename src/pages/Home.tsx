e1fab9bf620b49e05c79f7a7a466f99fe232c1a3
zhangyu <1015371461@qq.com>
2024-04-13 02:29:42 +0800

    feat: notFound page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
 import React, { FC } from 'react'
+import { List_PathNAME } from '../router'
+import styles from './Home.module.scss'
 import { useNavigate } from 'react-router-dom'
+import { Button, Typography } from 'antd'
 
+const { Title, Paragraph } = Typography
+
+// 主页面
 const Home = () => {
   const nav = useNavigate()
 
   function clickHandler() {
     console.log('to login page')
     // 使用第三方库实现
-    nav('/login')
+    nav(List_PathNAME)
   }
 
   return (
-    <div>
-      <p>home页面~~~~~~</p>
-      <button onClick={clickHandler}>跳转到登陆页面</button>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份，发布问卷 88 份，收到答卷 99 份</Paragraph>
+        <div>
+          <Button type="primary" size="large" onClick={clickHandler}>
+            开始使用
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,27 @@
 import React, { FC } from 'react'
+import { Button, Result } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { List_PathNAME } from '../router'
 
-const NotFound = () => {
-  return <div>NotFound页面~~~~~~~</div>
+const NotFound: FC = () => {
+  const nav = useNavigate()
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Button
+          type="primary"
+          onClick={() => {
+            nav(List_PathNAME)
+          }}
+        >
+          Back Home
+        </Button>
+      }
+    />
+  )
 }
 
 export default NotFound
